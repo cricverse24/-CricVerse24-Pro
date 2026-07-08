ffbce522e50db310452fa0dcc4a3c7750dc64303
cricverse24 <ak1045546@gmail.com>
2026-07-08 05:54:48 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "YAHAN_APNI_API_KEY",
+  authDomain: "YAHAN_AUTH_DOMAIN",
+  projectId: "YAHAN_PROJECT_ID",
+  storageBucket: "YAHAN_STORAGE_BUCKET",
+  messagingSenderId: "YAHAN_SENDER_ID",
+  appId: "YAHAN_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
