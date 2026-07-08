21b142f2d43bf4ca2cabeeffa5069751fe77bcda
cricverse24 <ak1045546@gmail.com>
2026-07-08 06:09:50 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,15 +1,16 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
-const firebaseConfig = {
-  apiKey: "YAHAN_APNI_API_KEY",
-  authDomain: "YAHAN_AUTH_DOMAIN",
-  projectId: "YAHAN_PROJECT_ID",
-  storageBucket: "YAHAN_STORAGE_BUCKET",
-  messagingSenderId: "YAHAN_SENDER_ID",
-  appId: "YAHAN_APP_ID"
-};
 
+ 
+const firebaseConfig = {
+  apiKey: "AIzaSyCXEnIpBRKZWHU_aXdTSXgem8J-HA-mB8w",
+  authDomain: "cricverse24-pro-ba5b1.firebaseapp.com",
+  projectId: "cricverse24-pro-ba5b1",
+  storageBucket: "cricverse24-pro-ba5b1.firebasestorage.app",
+  messagingSenderId: "200152574632",
+  appId: "1:200152574632:web:4d1aaf2ef80a3f1eb6b09d"
+};
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
