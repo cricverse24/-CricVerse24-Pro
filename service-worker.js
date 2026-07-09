fe367efb1679d01c24c55563448aa9e5fdf56796
cricverse24 <ak1045546@gmail.com>
2026-07-09 17:32:00 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,22 @@
+const CACHE_NAME = "cricverse24-v1";
 
+const urlsToCache = [
+  "/-CricVerse24-Pro/",
+  "/-CricVerse24-Pro/index.html"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
